ce27bd8baa2a07f1f9d56fa3684792d734fd03c6
sulphurninja <sulphurninja@gmail.com>
2025-05-15 05:22:09 +0530

    Commented out

---
+++ src/hooks/useWindowSize.ts
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+export default function useWindowSize() {
+  // Initialize with default values
+  const [windowSize, setWindowSize] = useState({
+    width: 1200,
+    height: 800,
+  });
+
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener('resize', handleResize);
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+  return windowSize;
+}
